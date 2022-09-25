634868b87b9530aa66473c705cc5fc3c8a2c8270
Emilio Lim <emilioarislim@gmail.com>
2022-09-25 07:34:49 -0700

    pi

--- index.js
+++ index.js
@@ -29,9 +29,23 @@ const client = Client.forTestnet().setOperator(operatorId, operatorKey);
 
 const supplyKey = PrivateKey.generate();
 const adminKey = PrivateKey.generate();
-CID = ["QmVV9uJYfyWQyRM4PATRxtYmWX7EeB9DNZtABFzFWgf33b"];
+CID = [
+  "QmVV9uJYfyWQyRM4PATRxtYmWX7EeB9DNZtABFzFWgf33b",
+  "QmfAwZ14jAVbQRSXuk9V1jv5cNovEDZjge71pqp7RoG5X7",
+  "QmbHE4A2dATJyMUh3sJJmiQm33DyyL9Vm3iQDsgPvYteAR",
+  "QmcfVR46rd42zinaFWCmJNKGpKN4bFzTrvw9ac6hkTYuKb",
+  "QmfE9oNwptnLVTTJM6T5YqAcbaPKRiJxMycswyMuRRqjvQ",
+];
 
 async function main() {
+  //Token Query to check if fee schedule is associated with Nft
+  // var tokenInfo = await new TokenInfoQuery()
+  //   .setTokenId(tokenId)
+  //   .execute(client);
+  // console.table(tokenInfo.customFees[0]);
+
+  //mint multiple nfts
+  nftLeaf = [];
   let nftCustomFee = await new CustomRoyaltyFee()
     .setNumerator(5)
     .setDenominator(10)
@@ -57,26 +71,25 @@ async function main() {
   let nftCreateSubmit = await nftCreateSign.execute(client);
   let nftCreateRx = await nftCreateSubmit.getReceipt(client);
   let tokenId = nftCreateRx.tokenId;
-  console.log(`Created NFT with Token ID ${tokenId}`);
+  for (var i = 0; i < CID.length; i++) {
+    nftLeaf[i] = await tokenMinterFcn(CID[i]);
 
-  //Token Query to check if fee schedule is associated with Nft
-  // var tokenInfo = await new TokenInfoQuery()
-  //   .setTokenId(tokenId)
-  //   .execute(client);
-  // console.table(tokenInfo.customFees[0]);
-
-  //mint multiple nfts
-  nftLeaf = [];
-
-  for (var i = 0; i < 5; i++) {
-    nftLeaf[i] = await tokenMinterFcn(CID[0]);
     console.log(
       `Create NFT ${tokenId} with serial: ${nftLeaf[i].serials[0].low}`
     );
   }
-
+  //console.log(nftLeaf);
   //auto associate nft with an account
   //later
+
+  // let associateTx = await new AccountUpdateTransaction()
+  //   .setAccountId() //input id
+  //   .setMaxAutomaticTokenAssociations(100)
+  //   .freezeWith(client)
+  //   .sign(); //input key
+  // let associationTxSubmit = await associateTx.execute(client);
+  // let associateRx = await associateTxSubmit.getReceipt(client);
+  // console.log(`User NFT Auto Association: ${associateRx.status}\n`);
 
   // //mint multiple nfts FUNCTION
   async function tokenMinterFcn(CID) {
@@ -96,157 +109,10 @@ main();
 //custom concerts
 //////////////
 
-// concerts = [
-//   {
-//     concert: {
-//       name:  ,
-//       tickets:
-
-//     },
-//   }
-// ]
-
-// // const {
-// //   default: MirrorChannel,
-// // } = require("@hashgraph/sdk/lib/channel/MirrorChannel");
-
-// require("dotenv").config();
-// // Configure accounts and client, and generate needed keys
-// const myAccountId = AccountId.fromString(process.env.OPERATOR_ID);
-// const myPrivateKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
-
-// const client = Client.forTestnet().setOperator(myAccountId, myPrivateKey);
-
-// const supplyKey = PrivateKey.generate();
-// //console.log(supplyKey);
-// const adminKey = PrivateKey.generate();
-// console.log("Supply Key: " + supplyKey);
-
-// CID = ["QmVV9uJYfyWQyRM4PATRxtYmWX7EeB9DNZtABFzFWgf33b"];
-
-// async function createUser() {
-//   //If we weren't able to grab it, we should throw a new error
-//   if (myAccountId == null || myPrivateKey == null) {
-//     throw new Error(
-//       "Environment variables myAccountId and myPrivateKey must be present"
-//     );
-//   }
-
-//   // Create our connection to the Hedera network
-//   // The Hedera JS SDK makes this really easy!
-//   const client = Client.forTestnet();
-
-//   client.setOperator(myAccountId, myPrivateKey);
-
-//   //Create new keys
-//   const newAccountPrivateKey = await PrivateKey.generateED25519();
-//   const newAccountPublicKey = newAccountPrivateKey.publicKey;
-
-//   const treasuryKey = newAccountPrivateKey;
-//   //Create a new account with 1,000 tinybar starting balance
-//   const newAccountTransactionResponse = await new AccountCreateTransaction()
-//     .setKey(newAccountPublicKey)
-//     .setInitialBalance(Hbar.fromTinybars(1000))
-//     .execute(client);
-
-//   // Get the new account ID
-//   const getReceipt = await newAccountTransactionResponse.getReceipt(client);
-//   const newAccountId = getReceipt.accountId;
-//   const treasuryId = newAccountId;
-//   console.log("The new account ID is: " + newAccountId);
-
-//   //Verify the account balance
-//   const accountBalance = await new AccountBalanceQuery()
-//     .setAccountId(newAccountId)
-//     .execute(client);
-
-//   console.log(
-//     "The new account balance is: " +
-//       accountBalance.hbars.toTinybars() +
-//       " tinybar."
-//   );
-
-//   //Create the transfer transaction
-//   const sendHbar = await new TransferTransaction()
-//     .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
-//     .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
-//     .execute(client);
-
-//   //Verify the transaction reached consensus
-//   const transactionReceipt = await sendHbar.getReceipt(client);
-//   console.log(
-//     "The transfer transaction from my account to the new account was: " +
-//       transactionReceipt.status.toString()
-//   );
-
-//   //Request the cost of the query
-//   const queryCost = await new AccountBalanceQuery()
-//     .setAccountId(newAccountId)
-//     .getCost(client);
-
-//   console.log("The cost of query is: " + queryCost);
-
-//   //Check the new account's balance
-//   const getNewBalance = await new AccountBalanceQuery()
-//     .setAccountId(newAccountId)
-//     .execute(client);
-
-//   console.log(
-//     "The account balance after the transfer is: " +
-//       getNewBalance.hbars.toTinybars() +
-//       " tinybar."
-//   );
-//   console.log(supplyKey);
-//   return {
-//     newAccountPrivateKey: newAccountPrivateKey,
-//     newAccountId: newAccountId,
-//   };
-// }
-
-// (async () => {
-//   let data = await createUser();
-//   console.log(
-//     data.newAccountPrivateKey,
-//     supplyKey,
-//     adminKey,
-//     data.newAccountId,
-//     CID
-//   );
-//   generateNFT(
-//     data.newAccountId,
-//     data.newAccountPrivateKey,
-//     adminKey,
-//     supplyKey,
-//     CID
-//   );
-// })();
-
-// async function generateNFT(treasuryId, treasuryKey, adminKey, supplyKey, CID) {
-//   console.log("this is a supply key" + supplyKey);
-//   let nftCustomFee = await new CustomRoyaltyFee()
-//     .setNumerator(1)
-//     .setDenominator(10)
-//     .setFeeCollectorAccountId(treasuryId)
-//     .setFallbackFee(new CustomFixedFee().setHbarAmount(new Hbar(200)));
-
-//   let nftCreate = await new TokenCreateTransaction()
-//     .setTokenName("Taylor Swift")
-//     .setTokenSymbol("TAYSFT")
-//     .setTokenType(TokenType.NonFungibleUnique)
-//     .setDecimals(0)
-//     .setInitialSupply(0)
-//     .setTreasuryAccountId(treasuryId)
-//     .setSupplyType(TokenSupplyType.Finite)
-//     .setMaxSupply(CID.length - 1)
-//     .setCustomFees([nftCustomFee])
-//     .setAdminKey(adminKey)
-//     .setSupplyType(supplyKey)
-//     .freezeWith(client)
-//     .sign(treasuryKey);
-
-//   let nftCreateSign = await nftCreate.sign(adminKey);
-//   let nftCreateSubmit = await nftCreateSign.execute(client);
-//   let nftCreateRx = await nftCreateSubmit.getReceipt(client);
-//   let tokenId = nftCreateRx.tokenId;
-//   console.log(`Created NFT with Token ID: ${tokenId}`);
-// }
+concerts = [
+  {
+    concert1: {
+      tickets: { nftLeaf },
+    },
+  },
+];

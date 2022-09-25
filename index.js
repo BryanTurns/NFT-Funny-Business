5730489cd3d591c08321538242f7caa8831f51ad
Emilio Lim <emilioarislim@gmail.com>
2022-09-25 00:42:05 -0700

    sex

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-require("dotenv").config();
+console.clear();
 const {
   AccountId,
   Client,
   PrivateKey,
+  TokenCreateTransaction,
+  TokenInfoQuery,
+  TokenType,
+  CustomRoyaltyFee,
+  CustomFixedFee,
+  TokenSupplyType,
+  TokenMintTransaction,
+  TokenBurnTransaction,
+  TransferTransaction,
   AccountCreateTransaction,
+  AccountUpdateTransaction,
+  TokenAssociateTransaction,
   AccountBalanceQuery,
   Hbar,
-  TransferTransaction,
 } = require("@hashgraph/sdk");
+// const {
+//   default: MirrorChannel,
+// } = require("@hashgraph/sdk/lib/channel/MirrorChannel");
 
+require("dotenv").config();
 // Configure accounts and client, and generate needed keys
 const myAccountId = AccountId.fromString(process.env.OPERATOR_ID);
 const myPrivateKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
-// const treasuryId = AccountId.fromString(process.env.TREASURY_ID);
-// const treasuryKey = PrivateKey.fromString(process.env.TREASURY_PVKEY);
 
 const client = Client.forTestnet().setOperator(myAccountId, myPrivateKey);
 
 const supplyKey = PrivateKey.generate();
+const adminKey = PrivateKey.generate();
 console.log("Supply Key: " + supplyKey);
 
 async function createUser() {
-  // If we weren't able to grab it, we should throw a new error
+  //If we weren't able to grab it, we should throw a new error
   if (myAccountId == null || myPrivateKey == null) {
     throw new Error(
       "Environment variables myAccountId and myPrivateKey must be present"
@@ -38,6 +51,7 @@ async function createUser() {
   const newAccountPrivateKey = await PrivateKey.generateED25519();
   const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
+  const treasuryKey = newAccountPrivateKey;
   //Create a new account with 1,000 tinybar starting balance
   const newAccountTransactionResponse = await new AccountCreateTransaction()
     .setKey(newAccountPublicKey)
@@ -47,7 +61,7 @@ async function createUser() {
   // Get the new account ID
   const getReceipt = await newAccountTransactionResponse.getReceipt(client);
   const newAccountId = getReceipt.accountId;
-
+  const treasuryId = newAccountId;
   console.log("The new account ID is: " + newAccountId);
 
   //Verify the account balance
@@ -98,35 +112,46 @@ async function createUser() {
   };
 }
 
-createUser().then((response) => {
-  userId = "0.0." + response.newAccountId.num.low;
-  console.log(userId);
-});
+(async () => {
+  let data = await createUser();
+  console.log(
+    data.newAccountId,
+    data.newAccountPrivateKey,
+    data.adminKey,
+    data.supplyKey
+  );
+  generateNFT(
+    data.newAccountId,
+    data.newAccountPrivateKey,
+    data.adminKey,
+    data.supplyKey
+  );
+})();
 
-// async function generateNFT() {
-//   let nftCustomFee = await new CustomRoyaltyFee()
-//     .setNumberator(1)
-//     .setDenominatior(10)
-//     .setCollectorAccountId(treasurryId)
-//     .setFallbackFee(new CustomFixedFee().setHbarAmount(new Hbar(200)));
+async function generateNFT(treasuryId, treasuryKey, adminKey, supplyKey) {
+  let nftCustomFee = await new CustomRoyaltyFee()
+    .setNumerator(1)
+    .setDenominator(10)
+    .setFeeCollectorAccountId(treasuryId)
+    .setFallbackFee(new CustomFixedFee().setHbarAmount(new Hbar(200)));
 
-//   let nftCreate = await new TokenCreateTransaction()
-//     .setTokenName("Taylor Swift")
-//     .setTokenSymbol("TAYSFT")
-//     .setTokenType(TokenType.NonFungibleUnique)
-//     .setDecimals(0)
-//     .setInitialSupply(0)
-//     .setTreasuryAccountId(treasuryId)
-//     .setSupplyType(TokenSupplyType.Finite)
-//     .setMaxSuply(100)
-//     .setCustomFees([nftCustomFee])
-//     .setAdminKey(supplyKey)
-//     .setSupplyType(supplyKey)
-//     .freezeWith(client)
-//     .sign(treasuryKey);
+  let nftCreate = await new TokenCreateTransaction()
+    .setTokenName("Taylor Swift")
+    .setTokenSymbol("TAYSFT")
+    .setTokenType(TokenType.NonFungibleUnique)
+    .setDecimals(0)
+    .setInitialSupply(0)
+    .setTreasuryAccountId(treasuryId)
+    .setSupplyType(TokenSupplyType.Finite)
+    .setMaxSupply(100)
+    .setCustomFees([nftCustomFee])
+    .setAdminKey(adminKey)
+    .setSupplyType(supplyKey)
+    .freezeWith(client)
+    .sign(treasuryKey);
 
-//   let nftCreateSign = await nftCreate.sign(adminKey);
-//   let nftCreateSubmit = await nftCreateSign.execute(client);
-//   let nftCreateRx = await nftCreateRx.tokenId;
-//   console.log(`Created NFT with Token ID: ${tokenId}`);
-// }
+  let nftCreateSign = await nftCreate.sign(adminKey);
+  let nftCreateSubmit = await nftCreateSign.execute(client);
+  let nftCreateRx = await nftCreateRx.tokenId;
+  console.log(`Created NFT with Token ID: ${tokenId}`);
+}
